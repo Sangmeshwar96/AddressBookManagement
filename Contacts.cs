ea6a615570a128b7edd7cf78465c972cbfee25ab
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-11-22 12:22:30 +0530

    Edit Contact Details

--- Contacts.cs
+++ Contacts.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AddressBook
 {
@@ -12,6 +13,12 @@ namespace AddressBook
         public string email;
         public string zip;
         public string phnNo;
+        List<Contacts> l = new List<Contacts>();
+
+        public Contacts()
+        {
+
+        }
 
         public Contacts(string frstName, string lastName, string address, string city, string state, string email, string zip, string phnNo)
         {
@@ -27,11 +34,121 @@ namespace AddressBook
         }
 
 
+        public void editDetails(string name)
+        {
+            Contacts contacts = new Contacts();
+            contacts = l[1];
+            if (contacts.frstName == name)
+            {
+                Console.WriteLine("enter the number of details you want to edit");
+
+                Console.WriteLine(" 1:frstName  2:lastName  3:address  4:city  5:state  6:email 7:zip  8:PhoneNumber ");
+                int num = Convert.ToInt32(Console.ReadLine()); //user enters the which detail should be updated
+                Console.WriteLine("enter the new detail");
+                string detail = Console.ReadLine();  //user enters the new detail
+
+                switch (num)
+                {
+                    case 1:
+                        contacts.frstName = detail;
+                        break;
+
+                    case 2:
+                        contacts.lastName = detail;
+                        break;
+                    case 3:
+                        contacts.address = detail;
+                        break;
+                    case 4:
+                        contacts.city = detail;
+                        break;
+                    case 5:
+                        contacts.state = detail;
+                        break;
+                    case 6:
+                        contacts.email = detail;
+                        break;
+                    case 7:
+                        contacts.zip = detail;
+                        break;
+                    case 8:
+                        contacts.phnNo = detail;
+                        break;
+                }
+
+
+            }
+            else
+                Console.WriteLine("there is no such user with entered first name");
+
+
+        }
+
+        public void enterDetails()
+        {
+            Console.WriteLine("Enter Contact Details");
+            Console.WriteLine("Enter First Name");
+            string frstName = Console.ReadLine();
+
+            Console.WriteLine("Enter last Name");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter Address ");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("Enter city Name");
+            string city = Console.ReadLine();
+
+            Console.WriteLine("Enter state Name");
+            string state = Console.ReadLine();
+
+            Console.WriteLine("Enter email id");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Enter Zip code");
+            string zip = Console.ReadLine();
+
+            Console.WriteLine("Enter Phone Number");
+            string phnNo = Console.ReadLine();
+
+            Contacts contacts = new Contacts(frstName, lastName, address, city, state, email, zip, phnNo);
+
+
+            new Contacts().l.Add(contacts);
+
+        }
 
         static void Main(string[] args)
         {
+            Contacts c = new Contacts();
             Console.WriteLine("Welcome to Address Book Management");
-            Console.ReadLine();
+            Console.WriteLine("Enter the choice");
+            Console.WriteLine("1: Enter contact details   2: edit contact details ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    c.enterDetails();
+                    break;
+
+                case 2:
+                    Console.WriteLine("enter the frstname to edit contact details ");
+                    string name = Console.ReadLine();
+                    c.editDetails(name);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+
+
+
+
+            }
+
+
+
+
         }
     }
 }
