4af1cadea3a4efca735fb6e48a472d4000d4c0df
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-11-22 12:14:30 +0530

    Address Book Management System Development

---
+++ Contacts.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Contacts
+    {
+        public string frstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public string email;
+        public string zip;
+        public string phnNo;
+
+        public Contacts(string frstName, string lastName, string address, string city, string state, string email, string zip, string phnNo)
+        {
+            this.frstName = frstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.email = email;
+            this.zip = zip;
+            this.phnNo = phnNo;
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Management");
+            Console.ReadLine();
+        }
+    }
+}
